6c361451c6c6901d50f067b313cebeec36c28b2a
mamassuda <matheus.massuda@unesp.br>
2026-08-25 12:02:10 -0300

    feat: adiciona materiais da Aula 25/08

---
+++ Aula_25_08/projeto01.R
@@ -0,0 +1,11 @@
+# 1. Carregar a biblioteca
+library(tidyverse)
+
+# 2. Criar ou ler dados (exemplo lendo um CSV)
+# Supondo que seu arquivo esteja dentro da pasta 'dados'
+df <- read_csv("dados/Pokemon_full.csv")
+
+# 3. Visualizar as primeiras linhas
+head(df)
+
+
